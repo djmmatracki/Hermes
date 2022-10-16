0b5bcb18b654db2279d6df1d65af00168fe7d192
Dominik <djmmatracki@gmail.com>
2022-10-16 13:18:35 +0200

    internal

---
+++ internal/parse.go
@@ -0,0 +1,58 @@
+package internal
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"runtime"
+
+	"github.com/qedus/osmpbf"
+)
+
+// Don't forget to sync with README.md
+
+func ListPoints() {
+	f, err := os.Open("greater-london-latest.osm.pbf")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	d := osmpbf.NewDecoder(f)
+
+	d.SetBufferSize(osmpbf.MaxBlobSize)
+
+	// start decoding with several goroutines, it is faster
+	err = d.Start(runtime.GOMAXPROCS(-1))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var nc, wc, rc uint64
+	for {
+		if v, err := d.Decode(); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		} else {
+			switch v := v.(type) {
+			case *osmpbf.Node:
+				// Process Node v.
+				nc++
+			case *osmpbf.Way:
+				// Process Way v.
+				wc++
+			case *osmpbf.Relation:
+				// Process Relation v.
+				rc++
+			default:
+				log.Fatalf("unknown type %T\n", v)
+			}
+		}
+	}
+
+	fmt.Printf("Nodes: %d, Ways: %d, Relations: %d\n", nc, wc, rc)
+	// Output:
+	// Nodes: 2729006, Ways: 459055, Relations: 12833
+}
